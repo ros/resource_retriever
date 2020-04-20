a0b44a592dc55408f5515a07900a39eced41724d
ahcorde <ahcorde@gmail.com>
2020-04-20 13:52:17 +0200

    Added common_linters and fixed tests
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- resource_retriever/include/resource_retriever/retriever.h
+++ resource_retriever/include/resource_retriever/retriever.h
@@ -1,32 +1,33 @@
-/*
- * Copyright (C) 2009, Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright 2009, Willow Garage, Inc. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the  Willow Garage, Inc. nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef RESOURCE_RETRIEVER_RETRIEVER_H
-#define RESOURCE_RETRIEVER_RETRIEVER_H
+#ifndef RESOURCE_RETRIEVER__RETRIEVER_H_
+#define RESOURCE_RETRIEVER__RETRIEVER_H_
 
 #include <cstdint>
 #include <memory>
@@ -39,13 +40,13 @@ typedef void CURL;
 
 namespace resource_retriever
 {
-
 class Exception : public std::runtime_error
 {
 public:
-  Exception(const std::string& file, const std::string& error_msg)
+  Exception(const std::string & file, const std::string & error_msg)
   : std::runtime_error("Error retrieving file [" + file + "]: " + error_msg)
-  {}
+  {
+  }
 };
 
 /**
@@ -55,7 +56,8 @@ struct MemoryResource
 {
   MemoryResource()
   : size(0)
-  {}
+  {
+  }
 
   std::shared_ptr<uint8_t> data;
   size_t size;
@@ -78,14 +80,14 @@ public:
    * \return The file, loaded into memory
    * \throws resource_retriever::Exception if anything goes wrong.
    */
-  MemoryResource get(const std::string& url);
+  MemoryResource get(const std::string & url);
 
 private:
   Retriever(const Retriever & ret) = delete;
 
-  CURL* curl_handle_;
+  CURL * curl_handle_;
 };
 
-} // namespace resource_retriever
+}  //  namespace resource_retriever
 
-#endif // RESOURCE_RETRIEVER_RETRIEVER_H
+#endif  //  RESOURCE_RETRIEVER__RETRIEVER_H_
--- resource_retriever/test/test.py
+++ resource_retriever/test/test.py
@@ -1,36 +1,59 @@
-import resource_retriever as r
+# Copyright 2020, Willow Garage, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-import os
-import rospkg
+import ament_index_python
+
 from nose.tools import raises
 
-rospack = rospkg.RosPack()
+import resource_retriever as r
+
 
 def test_get_by_package():
-    res = r.get("package://resource_retriever/test/test.txt")
+    res = r.get('package://resource_retriever/test/test.txt')
     assert len(res) == 1
     assert res == 'A'.encode()
 
-def test_get_large_file():
-    res_path = os.path.join(rospack.get_path("resource_retriever"), "test/large_filepy.dat")
-    with open(res_path, 'w') as f:
-        for _ in range(1024*1024*50):
-            f.write('A')
-    res = r.get("package://resource_retriever/test/large_filepy.dat")
-    assert len(res) == 1024*1024*50
 
 def test_http():
-    res = r.get("http://packages.ros.org/ros.key")
+    res = r.get('http://packages.ros.org/ros.key')
     assert len(res) > 0
+
 
 @raises(Exception)
 def test_invalid_file():
-    r.get("file://fail")
+    r.get('file://fail')
+
 
 @raises(Exception)
 def test_no_file():
-    r.get("package://roscpp")
+    r.get('package://roscpp')
 
-@raises(rospkg.common.ResourceNotFound)
+
+@raises(ament_index_python.PackageNotFoundError)
 def test_invalid_package():
-    r.get("package://invalid_package_blah/test.xml")
+    r.get('package://invalid_package_blah/test.xml')
