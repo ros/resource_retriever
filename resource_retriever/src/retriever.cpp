033e462f630a80c9e0b3e166d45e58312f258060
Jacob Perron <jacob@openrobotics.org>
2020-10-15 17:27:03 -0700

    Throw exception if package name is empty (#54)
    Otherwise, ament_index_cpp will throw a std::runtime_error.
    Throwing an exception from resource_retriever instead makes for a more useful error message.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- resource_retriever/src/retriever.cpp
+++ resource_retriever/src/retriever.cpp
@@ -105,6 +105,9 @@ MemoryResource Retriever::get(const std::string & url)
     }
 
     std::string package = mod_url.substr(0, pos);
+    if (package.empty()) {
+      throw Exception(url, "Package name must not be empty");
+    }
     mod_url.erase(0, pos);
     std::string package_path;
     try {
