a240335046eaff92bdf63041b8bb9144a874daba
Jacob Perron <jacob@openrobotics.org>
2022-01-12 14:25:19 -0800

    Fix include order for cpplint (#69)
    * Fix include order for cpplint
    
    Relates to https://github.com/ament/ament_lint/pull/324
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Use double-quotes
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- resource_retriever/test/test.cpp
+++ resource_retriever/test/test.cpp
@@ -26,12 +26,12 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <gtest/gtest.h>
-
-#include <resource_retriever/retriever.hpp>
-
 #include <string>
 #include <exception>
+
+#include "gtest/gtest.h"
+
+#include "resource_retriever/retriever.hpp"
 
 TEST(Retriever, getByPackage)
 {
