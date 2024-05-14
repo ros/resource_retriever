fe5469c6368d20c4fed4fc28f0361a07dd824786
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-05-14 15:02:39 +0200

    Allow spaces
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- resource_retriever/src/retriever.cpp
+++ resource_retriever/src/retriever.cpp
@@ -32,6 +32,7 @@
 
 #include <cstring>
 #include <memory>
+#include <regex>
 #include <string>
 #include <utility>
 #include <vector>
@@ -135,6 +136,9 @@ MemoryResource Retriever::get(const std::string & url)
 
     mod_url = "file://" + package_path + mod_url;
   }
+
+  // newer versions of curl do not accept spaces in URLs
+  mod_url = std::regex_replace(mod_url, std::regex(" "), "%20");
 
   curl_easy_setopt(curl_handle_, CURLOPT_URL, mod_url.c_str());
   curl_easy_setopt(curl_handle_, CURLOPT_WRITEFUNCTION, curlWriteFunc);
