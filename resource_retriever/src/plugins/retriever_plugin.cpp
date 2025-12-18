986fc89faa383b9a650820027a83912afa7ffb99
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-12-18 10:48:35 +0100

    Updated deprecated ament_index_cpp API
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- resource_retriever/src/plugins/retriever_plugin.cpp
+++ resource_retriever/src/plugins/retriever_plugin.cpp
@@ -28,6 +28,7 @@
 
 #include "resource_retriever/plugins/retriever_plugin.hpp"
 
+#include <filesystem>
 #include <string>
 #include <string_view>
 
@@ -76,14 +77,14 @@ std::string expand_package_url(const std::string & url)
       throw Exception(url, "Package name must not be empty");
     }
     mod_url.erase(0, pos);
-    std::string package_path;
+    std::filesystem::path package_path;
     try {
-      package_path = ament_index_cpp::get_package_share_directory(package);
+      ament_index_cpp::get_package_share_directory(package, package_path);
     } catch (const ament_index_cpp::PackageNotFoundError &) {
       throw Exception(url, "Package [" + package + "] does not exist");
     }
 
-    mod_url = "file://" + package_path + mod_url;
+    mod_url = "file://" + package_path.string() + mod_url;
   }
   return mod_url;
 }
